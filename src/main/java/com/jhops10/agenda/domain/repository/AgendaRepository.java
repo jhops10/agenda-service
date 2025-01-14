f148e539fadbf6c394e90b7575e94ec6adb13da2
João Paulo <joaowais@gmail.com>
2025-01-14 15:10:22 -0300

    Adiciona camada de serviço AgendaService

---
+++ src/main/java/com/jhops10/agenda/domain/repository/AgendaRepository.java
@@ -0,0 +1,14 @@
+package com.jhops10.agenda.domain.repository;
+
+import com.jhops10.agenda.domain.entity.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+
+    Optional<Agenda> findByDataHora(LocalDateTime horario);
+}
---
+++ src/main/java/com/jhops10/agenda/domain/service/AgendaService.java
@@ -0,0 +1,54 @@
+package com.jhops10.agenda.domain.service;
+
+import com.jhops10.agenda.domain.entity.Agenda;
+import com.jhops10.agenda.domain.entity.Paciente;
+import com.jhops10.agenda.domain.repository.AgendaRepository;
+import com.jhops10.agenda.exception.BusinessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class AgendaService {
+
+    private final AgendaRepository agendaRepository;
+    private final PacienteService pacienteService;
+
+    public AgendaService(AgendaRepository agendaRepository, PacienteService pacienteService) {
+        this.agendaRepository = agendaRepository;
+        this.pacienteService = pacienteService;
+    }
+
+    public List<Agenda> listarTodos() {
+        return agendaRepository.findAll();
+    }
+
+    public Optional<Agenda> buscarPorId(Long id) {
+        return agendaRepository.findById(id);
+    }
+
+    public Agenda salvar(Agenda agenda) {
+        Optional<Paciente> optPaciente = pacienteService.buscarPorId(agenda.getPaciente().getId());
+
+        if (optPaciente.isEmpty()) {
+            throw new BusinessException("Paciente não encontrado");
+        }
+
+        Optional<Agenda> optHorario = agendaRepository.findByDataHora(agenda.getDataHora());
+
+        if (optHorario.isPresent()) {
+            throw new BusinessException("Já existe um agendamento para esse horário");
+        }
+
+        agenda.setPaciente(optPaciente.get());
+        agenda.setDataCriacao(LocalDateTime.now());
+
+
+        return agendaRepository.save(agenda);
+    }
+
+}
